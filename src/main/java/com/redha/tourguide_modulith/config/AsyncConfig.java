36a96fca0102a19a6d2a5c075c646303732d3d16
MR-BOUBAKOUR <redha.boubakour@gmail.com>
2025-05-14 23:46:17 +0200

    executor.setCorePoolSize(cores * 10) & executor.setMaxPoolSize(cores * 20)

--- src/main/java/com/redha/tourguide_modulith/config/AsyncConfig.java
+++ src/main/java/com/redha/tourguide_modulith/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package com.redha.tourguide_modulith.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -7,6 +8,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
+@Slf4j
 @Configuration
 public class AsyncConfig {
 
@@ -16,8 +18,8 @@ public class AsyncConfig {
 
         int cores = Runtime.getRuntime().availableProcessors();
 
-        executor.setCorePoolSize(cores * 3);
-        executor.setMaxPoolSize(cores * 6);
+        executor.setCorePoolSize(cores * 10);
+        executor.setMaxPoolSize(cores * 20);
         executor.setQueueCapacity(500);
 
         executor.setThreadNamePrefix("TourGuide-");
